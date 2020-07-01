2b41545cfdeb93780eb5b0403db191e914c289b3
Bill Zhu <zhuwangz@sfu.ca>
2020-06-30 22:19:54 -0700

    r4r cogrounding model release

--- download_model.sh
+++ download_model.sh
@@ -7,4 +7,4 @@ wget https://www.dropbox.com/s/mc8iuav1g5buqfp/follower_r6r_bbw.zip?dl=0 -O task
 wget https://www.dropbox.com/s/ttwff1nv5sthd9t/follower_r8r_bbw.zip?dl=0 -O tasks/R8R/follower/follower_r8r_bbw.zip
 
 # babywalk(coground) models
-# proceeding
+wget https://www.dropbox.com/s/l2tnmb0ej6y5l0y/follower_coground_r4r_bbw.zip?dl=0 -O tasks/R4R/follower/follower_coground_r4r_bbw.zip
